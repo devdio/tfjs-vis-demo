f49784b50a77166fbce31cf4f092315352998b62
John Paul Ada <johnpaulada@gmail.com>
2019-03-22 02:42:47 +0800

    refactor: Moved Charts to Their Own Functions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react"
+import * as tfvis from '@tensorflow/tfjs-vis'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const renderScatterPlot = () => {
+  const series1 = Array(100).fill(0)
+    .map(y => Math.random() * 100 - (Math.random() * 50))
+    .map((y, x) => ({ x, y, }));
+
+  const series2 = Array(100).fill(0)
+    .map(y => Math.random() * 100 - (Math.random() * 150))
+    .map((y, x) => ({ x, y, }));
+
+  const series = ['First', 'Second'];
+  const scatterData = { values: [series1, series2], series }
+
+  const scatterPlotSurface = { name: 'Random Scatter Plot', tab: 'Charts' };
+  tfvis.render.scatterplot(scatterPlotSurface, scatterData);
 }
 
-export default App;
+const renderBarChart = () => {
+
+  const barChartData = [
+    { index: 0, value: 50 },
+    { index: 1, value: 100 },
+    { index: 2, value: 150 },
+  ];
+
+  // Get a surface
+  const barchartSurface = tfvis.visor().surface({ name: 'Boring Bar Chart', tab: 'Charts' });
+  
+  // Render a barchart on that surface
+  tfvis.render.barchart(barchartSurface, barChartData, {});
+}
+
+const App = () => {
+  useEffect(() => {
+    renderBarChart()
+    renderScatterPlot()
+  }, [])
+
+  return <div />
+}
+
+export default App
